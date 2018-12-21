58ea4472fa88b7c82a35ea5734eef94d88682e4f
jskkkim <horsequake@knu.ac.kr>
2018-12-22 01:57:54 +0900

    error

---
+++ COMP328-Team4/Assets/Scripts/Summer/Summer_ButtonManager.cs
@@ -0,0 +1,35 @@
+<<<<<<< HEAD
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Summer_ButtonManager : MonoBehaviour {
+
+    public void OnClick_Regame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void OnClick_Exit()
+    {
+
+    }
+}
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Summer_ButtonManager : MonoBehaviour {
+
+    public void OnClick_Regame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void OnClick_Exit()
+    {
+
+    }
+}
+>>>>>>> 4d88e1e6685ce6317777b5774857442832a5edb7
