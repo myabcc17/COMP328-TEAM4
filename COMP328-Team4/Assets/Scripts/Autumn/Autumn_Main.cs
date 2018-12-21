41624edd1b0e341259166ddf66eb07faedc4ec73
mycool0905 <mycool0905@gmail.com>
2018-12-21 13:37:42 +0900

    Autumn Timer Complete

--- COMP328-Team4/Assets/Scripts/Autumn/Autumn_Main.cs
+++ COMP328-Team4/Assets/Scripts/Autumn/Autumn_Main.cs
@@ -11,14 +11,26 @@ public class Autumn_Main : MonoBehaviour
     public Text[] leavesText = new Text[6]; // 버튼 6개에 달려있는 텍스트
     public GameObject[] fallingLeaves = new GameObject[8]; // 내려오는 잎들
     public Text[] leavesSequence = new Text[8]; // 내려오는 잎들 번호 배열
-    public int level; // level=1~level=3
+    private int level; // level=1~level=3
+    private int score; // 점수
+    private int readyToLevelUp; // 레벨업 준비 변수
+    private float fullTime; // 전체 시간
+    private float currentTime; // 타이머 시간
+    public Text timer;
 
     void Start()
     {
+        score = 0; 
         level = 1;
+        readyToLevelUp = 1;
+
+        currentTime = 60;
+        fullTime = 60;
+
         RandomLeaves();
         LeafSetActive();
         SetLeavesAligned();
+        StartCoroutine("LevelUp");
     }
 
     void RandomLeaves() // 버튼마다 잎 랜덤으로 배정하기
@@ -77,7 +89,7 @@ public class Autumn_Main : MonoBehaviour
 
     public void Correct() // 클릭을 맞췄을 경우
     {
-        for (int i = 0; i <= 6; i++) // 맞출경우 8개중 2~8번째꺼를 한칸씩 내려서 1~7번째 잎들로 맞춤
+        for (int i = 0; i < 7; i++) // 맞출경우 8개중 2~8번째꺼를 한칸씩 내려서 1~7번째 잎들로 맞춤
         {
             leavesSequence[i].text = leavesSequence[i + 1].text;
             string temp = (string)leavesSequence[i].text;
@@ -89,11 +101,42 @@ public class Autumn_Main : MonoBehaviour
         int m = ((int)(rand * 10)) % (level * 2);
         leavesSequence[7].text = leafNumber[m] + "";
         fallingLeaves[7].GetComponent<Image>().sprite = leafObj[leafNumber[m]];
-
+        
+        score++; // 클릭 성공 횟수
+        readyToLevelUp = 1;
     }
     
-    public void NotCorrect()
+    public void NotCorrect() // 실패 했을 경우
     {
+        StartCoroutine("BlockButton"); // 버튼 클릭 잠시 막기
+    }
 
+    IEnumerator BlockButton() // 버튼 2초가량 사라지게 만듬
+    {
+        for (int i = 0; i < 6; i++)
+            leaves[i].SetActive(false);
+
+        yield return new WaitForSeconds(2.0f);
+
+        for (int i = 0; i < (level*2); i++)
+            leaves[i].SetActive(true);
+    }
+
+    IEnumerator LevelUp()
+    {
+        yield return new WaitUntil(() => (score == 10 || score == 30) && readyToLevelUp == 1);
+        level++;
+        readyToLevelUp = 0;
+        LeafSetActive();
+        StartCoroutine("LevelUp");
+    }
+
+    void Update()
+    {
+        if(currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+            timer.text = "Time : " + currentTime.ToString("F");
+        }
     }
 }
