8426cbd40f52025c2950c026fcf310b0fb4d23d3
jskkkim <horsequake@knu.ac.kr>
2018-12-22 21:36:00 +0900

    winter add
    50%

---
+++ COMP328-Team4/Assets/Scripts/Winter/Winter_ButtonController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Winter_ButtonController : MonoBehaviour
+{
+    public Winter_Main main;
+    public Text myText;
+    public int count = 0;
+
+    public void OnClick()
+    {
+        main.ButtonCheck(myText);
+    }
+}
--- COMP328-Team4/Assets/Scripts/Winter/Winter_Main.cs
+++ COMP328-Team4/Assets/Scripts/Winter/Winter_Main.cs
@@ -1,16 +1,125 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Winter_Main : MonoBehaviour {
+public class Winter_Main : MonoBehaviour
+{
+    public Image Timer_bar;
+    public Image Fish;
+    public float var;
+    public Sprite[] ArrowImages = new Sprite[4];
+    public GameObject[] Arrows = new GameObject[4];
+    public Text[] ArrowText = new Text[4];
+    public Text[] ArrowSequence = new Text[4];
+    public int count = 0;
+    public AudioSource BGM;
+    public Text CountText;
+    public int buttonCount = 0;
+    // Use this for initialization
+    void Start()
+    {
+        Fish.fillAmount = 0;
+        CountText.text += count.ToString();
+        BGM.Play();
+        RandomArrows();
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+
+        Timer_bar.fillAmount -= Time.deltaTime / 60;
+        var = Time.time;
+        if (Timer_bar.fillAmount <= 0f)
+        {
+
+        }
+    }
+    public void RandomArrows()
+    {
+        float rand;
+        int n;
+        int[] check = new int[4];
+
+        
+        for(int i=0; i<4; i++)
+        {
+            rand = Random.value;
+            n = ((int)(rand * 10) % 4);
+            
+            Arrows[i].GetComponent<Image>().sprite = ArrowImages[n];
+            ArrowSequence[i].text = n + "";
+        }
+        ArrowSetActive();
+    }
+    public void Correct(int n)
+    {
+        if (n == 3)
+        {
+            Arrows[n].SetActive(false);
+            Increment_fish();
+            AddFish();
+            RandomArrows();
+        }
+        else
+        {
+            Increment_fish();
+            Arrows[n].SetActive(false);
+        }
+    }
+    public void ButtonCheck(Text text)
+    {
+        if (text.text == ArrowSequence[count].text)
+            Correct(count);
+        else
+        {
+            InCorrect();
+            count = 0;
+        }
+        count++;
+        if (count == 4)
+            count = 0;
+    }
+    public void InCorrect()
+    {
+        ArrowSetDeactivate();
+        RandomArrows();
+    }
+    void ArrowSetActive()
+    {
+        for (int i = 0; i < 4; i++)
+            Arrows[i].SetActive(true);
+    }
+    void ArrowSetDeactivate()
+    {
+        for (int i = 0; i < 4; i++)
+            Arrows[i].SetActive(false);
+    }
+    public void Increment_fish()
+    {
+        Fish.fillAmount += (float)0.25;
+    }
+
+    void AddFish()
+    {
+        Text text = GameObject.Find("PlusText").GetComponent<Text>();
+        text.color = Color.black;
+        StartCoroutine(FadeOut());
+        Fish.fillAmount = 0;
+        count++;
+        //text.color = new Color(50, 50, 50, 0);
+    }
+    IEnumerator FadeOut()
+    {
+        float fadeOutTime = 1f;
+        Text text = GameObject.Find("PlusText").GetComponent<Text>();
+        Color originalColor = text.color;
+        for(float i = 0.1f ; i <= fadeOutTime; i += Time.deltaTime)
+        {
+            text.color = Color.Lerp(originalColor, Color.clear, Mathf.Min(1, i/fadeOutTime));
+            yield return null;
+        }
+    }
+    
 }
