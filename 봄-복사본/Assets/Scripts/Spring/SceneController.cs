ab94d012406990b9f8643bf0859adc128b488156
leegw217 <37521568+leegw217@users.noreply.github.com>
2018-12-22 17:45:39 +0900

    봄 백업
    봄 백업

---
+++ 봄-복사본/Assets/Scripts/Spring/SceneController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void onClickSpring()
+    {
+        SceneManager.LoadScene("Spring_Main");
+    }
+
+    public void onClickSummer()
+    {
+        SceneManager.LoadScene("Summer_Main");
+    }
+
+    public void onClickAutumn()
+    {
+        SceneManager.LoadScene("Autumn_Main");
+    }
+
+    public void onClickWinter()
+    {
+        SceneManager.LoadScene("Winter_Main");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
